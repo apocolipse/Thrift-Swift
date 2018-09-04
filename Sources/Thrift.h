2c35c678ebf0873f7dc4306e05c7ef550f2c3c92
Bojan Dimovski <bojand@gmail.com>
2018-09-04 14:14:57 +0200

    Add the framework public header.

---
+++ Sources/Thrift.h
@@ -0,0 +1,19 @@
+//
+//  Thrift.h
+//  Thrift
+//
+//  Created by Bojan Dimovski on 04/09/2018.
+//  Copyright © 2018 Bojan Dimovski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Thrift.
+FOUNDATION_EXPORT double ThriftVersionNumber;
+
+//! Project version string for Thrift.
+FOUNDATION_EXPORT const unsigned char ThriftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Thrift/PublicHeader.h>
+
+
